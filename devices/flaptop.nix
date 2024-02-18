b8370e8375094e3ad5b4b72f55c3224fcffb8a25
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-02-18 22:10:57 +0100

    update config for flaptop

--- devices/flaptop.nix
+++ devices/flaptop.nix
@@ -12,16 +12,19 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
+  # Enable Virtualbox
+  virtualisation.virtualbox.host.enable = true;
+
   # Configure for intel CPU
   nixpkgs.hostPlatform = "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true;
   boot.kernelModules = [ "kvm-intel" ];
-  boot.kernelParams = [ "i915.force_probe=46a6" "mem_sleep_default=deep" ];
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
 
   # Enable kernel modules for peripherics
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "i915" ];
   boot.extraModulePackages = [ ];
 
   # Configure suspend-then-hibernate
@@ -66,47 +69,15 @@
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 
   # Configure integrated GPU
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
+  hardware.opengl.enable = true;
+
+  environment.variables = {
+    VDPAU_DRIVER = (lib.mkDefault "va_gl");
   };
 
-  # Configure Nvidia Graphic Card
-  /* hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    };
-
-    hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  }; */
+  hardware.opengl.extraPackages = with pkgs; [
+    (if (lib.versionOlder (lib.versions.majorMinor lib.version) "23.11") then vaapiIntel else intel-vaapi-driver)
+    libvdpau-va-gl
+    intel-media-driver
+  ];
 }
